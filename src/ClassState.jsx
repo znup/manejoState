06fb981b57aeee37830d65a339cc65c2a47518af
SirAdan <lpelcastre@bomuca.com>
2023-01-20 12:25:04 -0800

    add ClassState

---
+++ src/ClassState.jsx
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+
+class ClassState extends Component {
+  render() {
+    return (
+      <>
+        <h3>Eliminar ClassState</h3>
+        <p>Escribe el código de seguridad</p>
+        <input type="text" placeholder="Código de seguridad" />
+        <button>Comprobar</button>
+      </>
+    );
+  }
+}
+
+export { ClassState };
---
+++ src/UseState.jsx
@@ -0,0 +1,12 @@
+const UseState = () => {
+  return (
+    <>
+      <h3>Eliminar useState</h3>
+      <p>Escribe el código de seguridad</p>
+      <input type="text" placeholder="Código de seguridad" />
+      <button>Comprobar</button>
+    </>
+  );
+};
+
+export { UseState };
